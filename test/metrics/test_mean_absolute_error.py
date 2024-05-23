399f3abde11cc648862e63cc7e9f466ae4b96b29
Rafael Greca <rgvieira97@gmail.com>
2024-05-23 10:22:11 -0300

    adding test for the mean absolute error metric and updating mean squared error test

--- test/metrics/test_mean_absolute_error.py
+++ test/metrics/test_mean_absolute_error.py
@@ -1,11 +1,11 @@
 import unittest
 import numpy as np
-from sklearn.metrics import mean_squared_error as SkMSE
+from sklearn.metrics import mean_absolute_error as SkMAE
 from sklearn.linear_model import LinearRegression as SkLinearRegression
-from scratchml.metrics import mean_squared_error
+from scratchml.metrics import mean_absolute_error
 from test.utils import generate_regression_dataset, repeat
 
-class Test_MeanSquaredError(unittest.TestCase):
+class Test_MeanAbsoluteError(unittest.TestCase):
     @repeat(10)
     def test_1(self):
         X, y = generate_regression_dataset(
@@ -20,10 +20,10 @@ class Test_MeanSquaredError(unittest.TestCase):
 
         sklr_prediction = sklr.predict(X)
 
-        sklr_score = SkMSE(y, sklr_prediction, squared=True)
-        score = mean_squared_error(y, sklr_prediction, derivative=False)
+        sklr_score = SkMAE(y, sklr_prediction)
+        score = mean_absolute_error(y, sklr_prediction, derivative=False)
 
         assert np.abs(score - sklr_score) < 1
-
+        
 if __name__ == "__main__":
     unittest.main(verbosity=2)
