1e8a132e3865212104f9df1614d32d1b7b848bc3
Rafael Greca <rgvieira97@gmail.com>
2024-05-23 15:13:03 -0300

    adding test for the r squared metric

---
+++ test/metrics/test_r_squared.py
@@ -0,0 +1,29 @@
+import unittest
+import numpy as np
+from sklearn.metrics import r2_score as SkR2
+from sklearn.linear_model import LinearRegression as SkLinearRegression
+from scratchml.metrics import r_squared
+from test.utils import generate_regression_dataset, repeat
+
+class Test_RSquared(unittest.TestCase):
+    @repeat(10)
+    def test_1(self):
+        X, y = generate_regression_dataset(
+            n_samples=10000,
+            n_features=10,
+            n_targets=1
+        )
+
+        sklr = SkLinearRegression()
+
+        sklr.fit(X, y)
+
+        sklr_prediction = sklr.predict(X)
+
+        sklr_score = SkR2(y, sklr_prediction)
+        score = r_squared(y, sklr_prediction)
+
+        assert np.abs(score - sklr_score) < 1
+        
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
