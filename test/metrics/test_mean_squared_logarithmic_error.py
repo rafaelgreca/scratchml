39232b1f7943afb8d70dddcdb85b7aad092d951d
Rafael Greca <rgvieira97@gmail.com>
2024-05-23 14:59:36 -0300

    adding test for mean squared log error

---
+++ test/metrics/test_mean_squared_logarithmic_error.py
@@ -0,0 +1,32 @@
+import unittest
+import numpy as np
+from sklearn.metrics import mean_squared_log_error as SkMSLE
+from sklearn.linear_model import LinearRegression as SkLinearRegression
+from scratchml.metrics import mean_squared_logarithmic_error
+from test.utils import generate_regression_dataset, repeat
+
+class Test_MeanSquaredLogarithmicError(unittest.TestCase):
+    @repeat(10)
+    def test_1(self):
+        X, y = generate_regression_dataset(
+            n_samples=10000,
+            n_features=10,
+            n_targets=1
+        )
+
+        sklr = SkLinearRegression()
+
+        # MSLE only accepts positive targets
+        y = np.abs(y)
+
+        sklr.fit(X, y)
+
+        sklr_prediction = sklr.predict(X)
+
+        sklr_score = SkMSLE(y, sklr_prediction)
+        score = mean_squared_logarithmic_error(y, sklr_prediction, derivative=False)
+
+        assert np.abs(score - sklr_score) < 1
+        
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
