6907d04e19d915e22cc55dd0d98f681bfa859bee
Rafael Greca <rgvieira97@gmail.com>
2024-05-22 21:07:28 -0300

    improving the quality of the linear regression code + adding max_iter, loss_function, and metric parameters

--- scratchml/models/linear_regression.py
+++ scratchml/models/linear_regression.py
@@ -8,54 +8,139 @@ class LinearRegression(object):
         self,
         learning_rate: float,
         tol: float,
+        max_iters: int = -1,
+        loss_function: str = "mse",
         n_jobs: int = None
     ) -> None:
+        """
+        Creates a Linear Regression instance.
+
+        Args:
+            learning_rate (float): the learning rate used to train the model.
+            tol (float): the tolerance of the difference between two sequential
+                lossed, which is used as a stopping criteria.
+            max_iters (int, optional): the maximum number of iterations
+                during the model training. -1 means that no maximum
+                iterations is used. Defaults to -1.
+            loss_function (str, optional): the loss function to be used.
+                Defaults to "mse".
+            n_jobs (int, optional): the number of jobs to be used.
+                -1 means that all CPUs are used to train the model. Defaults to None.
+        """
         self.n_jobs = n_jobs
         self.coef_ = None
         self.intercept_ = None
         self.n_features_in = None
         self.lr = learning_rate
         self.tol = tol
+        self.max_iters = max_iters
+        self.loss_function = loss_function
+        self._valid_loss_functions = ["mse"]
+        self._valid_metrics = ["r_squared"]
     
     def fit(
         self,
         X: np.ndarray,
         y: np.ndarray
     ) -> None:
+        """
+        Function responsible for training the Linear Regression model.
+
+        Args:
+            X (np.ndarray): the features.
+            y (np.ndarray): the targets of the features.
+        """
         self.n_features_in_ = X.shape[1]
         X = convert_array_numpy(X)
         y = convert_array_numpy(y)
+        
+        # validating the max_iters value
+        if self.max_iters < -1 or self.max_iters == 0:
+            return ValueError("Invalid value for 'max_iters'. Must be -1 or >= 1.\n")
+        
+        # validating the loss_function value
+        try:
+            assert self.loss_function in self._valid_loss_functions
+        except AssertionError:
+            return ValueError(
+                f"Invalid value for 'loss_function'. Must be {self._valid_loss_functions}.\n"
+            )
         
         self.intercept_ = 0.0
         self.coef_ = np.zeros(X.shape[1])
         last_losses = np.zeros(X.shape[1]) + np.inf
 
+        if self.max_iters == -1:
+            count = -99
+        else:
+            count = 1
+
+        # training loop
         while True:
+            # making the prediction
             y_hat = self.predict(X)
 
-            loss = mse(y, y_hat, derivative=True)
-            
-            derivative_coef = (np.matmul(X.T, loss)) / y.shape[0]
-            derivative_intercept = (np.sum(loss)) / y.shape[0]
+            # calculating the loss according to the chosen
+            # loss function
+            if self.loss_function == "mse":
+                loss = mse(y, y_hat, derivative=True)
+                derivative_coef = (np.matmul(X.T, loss)) / y.shape[0]
+                derivative_intercept = (np.sum(loss)) / y.shape[0]
 
+            # updating the coefficients
             self.coef_ = self.coef_ - (self.lr * derivative_coef)
             self.intercept_ = self.intercept_ - (self.lr * derivative_intercept)
 
-            if np.max(np.abs(last_losses)) < self.tol:
+            # stopping criteria 
+            if (np.max(np.abs(last_losses)) < self.tol) or \
+                (count >= self.max_iters):
                 break
             
             last_losses = derivative_coef
+
+            if self.max_iters != -1:
+                count += 1
     
     def predict(
         self,
         X: np.ndarray
     ) -> np.ndarray:
+        """
+        Uses the trained model to predict the targets of a given
+        set (also called features).
+
+        Args:
+            X (np.ndarray): the features.
+
+        Returns:
+            np.ndarray: the predicted targets.
+        """
         return np.matmul(X, self.coef_) + self.intercept_
     
     def score(
         self,
         X: np.ndarray,
-        y: np.ndarray
+        y: np.ndarray,
+        metric: str = "r_squared"
     ) -> np.float32:
+        """
+        Calculates the score of the model on a given test for a
+        determined metric.
+
+        Args:
+            X (np.ndarray): the features.
+            y (np.ndarray): the targets of the features.
+            metric (str, optional): which metric to use. Defaults to "r_squared".
+
+        Returns:
+            np.float32: the score achieved by the model.
+        """
+        try:
+            assert metric in self._valid_metrics
+        except AssertionError:
+            return f"Invalid value for 'metric'. Must be {self._valid_metrics}.\n"
+        
         y_hat = self.predict(X)
-        return r_squared(y, y_hat)
+
+        if metric == "r_squared":
+            return r_squared(y, y_hat)
